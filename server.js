ac610361fd2225d458adf7a897dddeec167dde4c
Patrick Klitzke <philologos14@gmail.com>
2016-06-29 16:03:48 +0900

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(3000, '0.0.0.0', function (err, result) {
   if (err) {
     console.log(err);
   }
