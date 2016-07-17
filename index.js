53df08d2e68484b5035f59766b02340c9ec3ec86
Patrick Klitzke <philologos14@gmail.com>
2016-07-17 19:53:37 +0900

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
